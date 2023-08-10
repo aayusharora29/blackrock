ea95a5735a5e003ad6f1c9190e1b00fcfc795ab1
Aayush Arora <aayusharora29@gmail.com>
2023-08-10 19:26:09 +0530

    init

--- finedify-app/src/App.js
+++ finedify-app/src/App.js
@@ -2,16 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import MainHomepage from './components/homepage/homepage';
+import VideosForMales from './components/homepage/VideosForMales';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-        <MainHomepage />
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+          <MainHomepage />
+          <VideosForMales />
 
-        <h1>Finedify App</h1>
+          <h1>Finedify App</h1>
 
         </>
       </header>
